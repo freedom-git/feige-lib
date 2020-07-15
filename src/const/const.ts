89cb47d9e7a969ea7859bcf01b01171baac23983
zhangzhijun <zhangzhijun@58.com>
2020-07-15 23:36:22 +0800

    1.13.0

--- src/const/const.ts
+++ src/const/const.ts
@@ -75,27 +75,27 @@ class Const {
     BUSINESS = {
         BASE: {
             ID: 'base',
-            PRICE: 0.01,
+            PRICE: 15,
             NAME: '核心功能',
-            PERMANENT_PRICE: 0.1,
+            PERMANENT_PRICE: 500,
         },
         WAITER: {
             ID: 'waiter',
-            PRICE: 0.01,
+            PRICE: 10,
             NAME: '服务员功能',
-            PERMANENT_PRICE: 0.1,
+            PERMANENT_PRICE: 300,
         },
         KDS: {
             ID: 'kds',
-            PRICE: 0.01,
+            PRICE: 15,
             NAME: '后厨显示功能',
-            PERMANENT_PRICE: 0.1,
+            PERMANENT_PRICE: 500,
         },
-        BASE_TAKEAWAY: {
-            ID: 'baseTakeaway',
-            PRICE: 0.02,
-            NAME: '基础外卖功能',
-        },
+        // BASE_TAKEAWAY: {
+        //     ID: 'baseTakeaway',
+        //     PRICE: 0.02,
+        //     NAME: '基础外卖功能',
+        // },
     };
     PROBATION = 1; // 公共试用期，当具体模块下没有该字段时，取这里的数据，单位：月
     LANGUAGE = [
