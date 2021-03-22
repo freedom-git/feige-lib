6c3b60451e13eab678c3a3e4605d0867d87e39e6
Zhang Zhijun <zhangzhijun@Zhangs-MacBook-Air.local>
2021-03-22 19:34:14 +0800

    添加接通支付的字段枚举

---
+++ src/enum/yeepay.enum.ts
@@ -0,0 +1,13 @@
+// UNI_CREDIT_CODE=统一社会信用代码证；CORP_CODE＝营业执照
+export enum YeepayMerCertTypeEnum {
+    UNI_CREDIT_CODE = 'UNI_CREDIT_CODE',
+    CORP_CODE = 'CORP_CODE',
+}
+
+//LEGAL_PERSON:法人 CONTROLLER:实际控制人 AGENT:代理人 OTHER:其他人
+export enum YeepayContactTypeEnum {
+    LEGAL_PERSON = 'LEGAL_PERSON',
+    CONTROLLER = 'CONTROLLER',
+    AGENT = 'AGENT',
+    OTHER = 'OTHER',
+}
