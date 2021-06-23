75cce4b59d76cd2269fdff4f7798dd0745d4b9bb
includejs@163.com <includejs@163.com>
2021-06-23 15:15:29 +0800

    用户的6个状态

---
+++ src/enum/user-status.enum.ts
@@ -0,0 +1,8 @@
+export enum UserStatusEnum {
+    New ='New',
+    Abandoned = 'Abandoned',
+    Purchased = 'Purchased',
+    WillLost = 'WillLost',
+    LostWithoutPurchased = 'LostWithoutPurchased',
+    LostWithPurchased = 'LostWithPurchased',
+}
