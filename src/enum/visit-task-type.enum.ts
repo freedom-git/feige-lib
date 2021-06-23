1a042897d5ba6764445443ce3c4966f7f2478c90
includejs@163.com <includejs@163.com>
2021-06-23 15:30:23 +0800

    任务的4种类型

---
+++ src/enum/visit-task-type.enum.ts
@@ -0,0 +1,6 @@
+export enum VisitTaskTypeEnum {
+    NewUserVisit ='NewUserVisit',
+    CustomizeVisit = 'CustomizeVisit',
+    WillLostVisit = 'WillLostVisit',
+    RegularVisit = 'RegularVisit'
+}
