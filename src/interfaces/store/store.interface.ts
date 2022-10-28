91dff82c17d22469c8f7be463726017fc3c6932a
zhangzhijun <freedom.zhangzhijun@gmail.com>
2022-10-28 22:23:04 +0800

    后厨打印添加 addTableNumBeforeDishName

--- src/interfaces/store/store.interface.ts
+++ src/interfaces/store/store.interface.ts
@@ -185,6 +185,7 @@ export interface ChefPrinter {
     countFront?: boolean;
     showWaiterName?: boolean;
     showSalesmanName?: boolean;
+    addTableNumBeforeDishName?: boolean;
 }
 
 export interface LabelPrinter {
