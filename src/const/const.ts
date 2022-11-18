402cc0c993413c5d9d6cbd9ce21652f6d27f70bc
zhangzhijun <freedom.zhangzhijun@gmail.com>
2022-11-18 18:38:50 +0800

    fix typo

--- src/const/const.ts
+++ src/const/const.ts
@@ -174,7 +174,7 @@ class Const {
             speechCode: 'en-US',
             dateFormat: DateFormat.DAY_MONTH_YEAR,
             defaultAreaCode: '61',
-            codepage: 'pc850',
+            codepage: 'cp850',
             defaultPrintEncodeType: EncodeTypeEnum.CODEPAGE,
         },
         {
