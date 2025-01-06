459c1457c07a09a6d2a2bcec115e9fce9b074c84
zhangzhijun <freedom.zhangzhijun@gmail.com>
2025-01-06 10:55:30 +0800

    update lib

--- src/enum/from.enum.ts
+++ src/enum/from.enum.ts
@@ -1,6 +1,12 @@
 export enum FromEnum {
     'Baidu' = '百度搜索',
+    'Google' = '谷歌',
+    'Bing' = '必应',
+    'RedBook' = '小红书',
     'Zhihu' = '知乎',
+    'TicTok' = 'Tictok',
+    'Facebook' = 'Facebook',
+    'Twitter' = 'Twitter',
     'Agent' = '代理商',
     'Friends' = '朋友介绍',
     'AppStore' = 'APP市场',
